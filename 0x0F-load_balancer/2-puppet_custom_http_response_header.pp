8f59b2e74fbbd8b565683ce4f055aaafe1139e9c
skd-alt <sipho@skanalytics.co.za>
2023-12-06 01:44:20 -0800

    puppet install

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,33 +1,17 @@
-# Install NginX
-# With puppet
-
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+# Installs Nginx and custom HTTP response headers
+exec { '/usr/bin/env apt-get -y update' : }
+-> package { 'nginx':
+  ensure => installed,
 }
-
-package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+-> file { '/var/www/html/index.html' :
+  content => 'Holberton School!',
 }
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
-}
-
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
-}
-
-file_line { 'add header' :
+-> file_line { 'add header' :
   ensure => present,
-  path   => '/etc/nginx/sites-enabled/default',
+  path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By ${hostname};",
   after  => 'server_name _;',
 }
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+-> service { 'nginx':
+  ensure => running,
 }
