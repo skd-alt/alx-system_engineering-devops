07051e2fa518c872b52307ab9a2d5c8b06e65570
skd-alt <sipho@skanalytics.co.za>
2024-01-23 22:26:19 +0000

    Json of all

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A JSONPlacholder to get employee data """
+import json
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+
+    users = '{}users/'.format(url)
+    res = requests.get(users)
+    json_o = res.json()
+    for u in json_o:
+        name = u.get('username')
+
+        todos = '{}todos?userId={}'.format(url, u.get('id'))
+        res2 = requests.get(todos)
+        tasks = res2.json()
+        user_task = []
+        for task in tasks:
+            dict_task = {"task": task.get('title'),
+                         "completed": task.get('completed'),
+                         "username": name}
+            user_task.append(dict_task)
+
+        final_task = {str(u.get('id')): user_task}
+        filename = 'todo_all_employees.json'
+        with open(filename, mode='a') as f:
+            json.dump(final_task, f)
