f0e773570b2b2ce41f873ba7f7db58b081e3e90c
skd-alt <sipho@skanalytics.co.za>
2024-04-09 21:14:25 +0000

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,7 +19,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
     results = response.json()
-
+    
     try:
         return results.get('data').get('subscribers')
 
